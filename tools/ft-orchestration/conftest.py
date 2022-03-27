bed5f063b585a84a54e6a7f0b60e76685f26baba
Dominik Tran <tran@cesnet.cz>
2022-03-27 20:24:35 +0000

    4874 - ft-orchestration - introduce pytest skeleton

---
+++ tools/ft-orchestration/conftest.py
@@ -0,0 +1,12 @@
+"""
+Author(s): Dominik Tran <tran@cesnet.cz>
+
+Copyright: (C) 2022 CESNET, z.s.p.o.
+SPDX-License-Identifier: BSD-3-Clause
+
+Pytest conftest file.
+"""
+
+
+def pytest_addoption(parser):  # pylint: disable=unused-argument
+    """Add pytest options. Currently empty."""
