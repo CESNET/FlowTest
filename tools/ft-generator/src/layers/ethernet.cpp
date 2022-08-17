96b156bcb3aa45aaf119bef51954321d9a70b92e
Michal Sedlak <sedlakm@cesnet.cz>
2022-08-17 14:02:41 +0200

    4720 - Add ethernet layer

---
+++ tools/ft-generator/src/layers/ethernet.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Ethernet layer planner
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "ethernet.h"
+#include "../packetflowspan.h"
+
+#include <pcapplusplus/EthLayer.h>
+
+namespace generator {
+
+Ethernet::Ethernet(MacAddress macSrc, MacAddress macDst) :
+	_macSrc(macSrc), _macDst(macDst)
+{
+}
+
+void Ethernet::PlanFlow(Flow& flow)
+{
+	PacketFlowSpan packetsSpan(&flow, true);
+
+	for (auto& packet: packetsSpan) {
+		Packet::layerParams params;
+		packet._layers.emplace_back(std::make_pair(this, params));
+	}
+}
+
+void Ethernet::Build(pcpp::Packet& packet, Packet::layerParams& params, Packet& plan)
+{
+	pcpp::EthLayer *ethLayer;
+
+	(void) params;
+
+	if (plan._direction == Direction::Forward) {
+		ethLayer = new pcpp::EthLayer(_macSrc, _macDst);
+	} else {
+		ethLayer = new pcpp::EthLayer(_macDst, _macSrc);
+	}
+
+	packet.addLayer(ethLayer, true);
+}
+
+} // namespace generator
---
+++ tools/ft-generator/src/layers/ethernet.h
@@ -0,0 +1,57 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Ethernet layer planner
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include "../flow.h"
+#include "../layer.h"
+#include "../packet.h"
+
+#include <pcapplusplus/MacAddress.h>
+
+namespace generator {
+
+/**
+ * @brief A representation of an Ethernet layer.
+ */
+class Ethernet : public Layer {
+public:
+
+	using MacAddress  = pcpp::MacAddress;
+
+	/**
+	 * @brief Construct a new Ethernet object
+	 *
+	 * @param macSrc Source MAC address
+	 * @param macDst Destination MAC address
+	 */
+	Ethernet(MacAddress macSrc, MacAddress macDst);
+
+	/**
+	 * @brief Plan Ethernet layer
+	 *
+	 * @param flow Flow to plan.
+	 */
+	virtual void PlanFlow(Flow& flow) override;
+
+	/**
+	 * @brief Build Ethernet layer in packet pcpp::Packet
+	 *
+	 * @param packet Packet to build.
+	 * @param params Layers parameters
+	 * @param plan   Packet plan.
+	 */
+	virtual void Build(pcpp::Packet& packet, Packet::layerParams& params, Packet& plan) override;
+
+private:
+	MacAddress _macSrc;
+	MacAddress _macDst;
+};
+
+} // namespace generator
