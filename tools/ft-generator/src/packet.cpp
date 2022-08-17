885065a2cc62fe61e7e3e507dca8ee9a3259833c
Michal Sedlak <sedlakm@cesnet.cz>
2022-08-17 14:02:41 +0200

    4720 - Add packet class

---
+++ tools/ft-generator/src/packet.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Packet interface
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "packet.h"
+
+namespace generator {
+
+} // namespace generator
---
+++ tools/ft-generator/src/packet.h
@@ -0,0 +1,50 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Packet interface
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include <map>
+#include <variant>
+#include <vector>
+#include <utility>
+#include <iostream>
+
+namespace generator {
+
+class Layer;
+
+/**
+ * @brief Packet direction
+ *
+ * @note Completed packet has to contains FORWARD or REVERSE direction.
+ */
+enum class Direction {
+	Unknown,
+	Forward,
+	Reverse,
+};
+
+
+/**
+ * @brief Class representing the packet planner interface
+ */
+class Packet {
+public:
+	using layerValue  = std::variant<std::monostate, uint64_t>;
+	using layerParams = std::map<int, layerValue>;
+	using layer       = std::pair<Layer *, layerParams>;
+
+	Direction _direction = Direction::Unknown;    //< Packet direction
+	uint64_t _timestamp = 0;                      //< Timestamp 32b (seconds) | 32b (microseconds)
+	size_t _size = 0;                             //< Planned packet size (IP header and above)
+	bool _isFinished = false;                     //< Do not add more layers to packet
+	std::vector<layer> _layers;                   //< Packet protocol layers in order
+};
+
+} // namespace generator
