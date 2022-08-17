2eda5e620aedae442c64646f91ccd506583fe307
Michal Sedlak <sedlakm@cesnet.cz>
2022-08-17 14:02:41 +0200

    4720 - Add packet flow span iterator class

---
+++ tools/ft-generator/src/packetflowspan.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Packet iterator
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "packetflowspan.h"
+
+namespace generator {
+
+PacketFlowSpan::iterator::iterator(packetIterator wrapper, packetIterator wrapperEnd, bool getOnlyAvailable) :
+	_wrapper(wrapper), _wrapperEnd(wrapperEnd), _getOnlyAvailable(getOnlyAvailable)
+{
+	GetElement(false);
+}
+
+void PacketFlowSpan::iterator::GetElement(bool incrementOperator)
+{
+	if (incrementOperator == true) {
+		++_wrapper;
+	}
+
+	while (_getOnlyAvailable && _wrapper != _wrapperEnd && (*_wrapper)._isFinished) {
+		++_wrapper;
+	}
+}
+
+std::pair<size_t, size_t> PacketFlowSpan::getAvailableDirections()
+{
+	size_t fwd = _flow->_fwdPackets;
+	size_t rev = _flow->_revPackets;
+
+	for (auto& packet : _flow->_packets) {
+		if (packet._direction == Direction::Forward) {
+			assert(fwd > 0 && "value underflow");
+			fwd--;
+		}
+
+		if (packet._direction == Direction::Reverse) {
+			assert(rev > 0 && "value underflow");
+			rev--;
+		}
+	}
+
+	return std::make_pair(fwd, rev);
+}
+
+} // namespace generator
