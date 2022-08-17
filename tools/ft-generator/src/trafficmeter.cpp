a315465f0fd4528e907512a09ee359e6b20ad384
Michal Sedlak <sedlakm@cesnet.cz>
2022-08-17 14:02:41 +0200

    4720 - Add traffic meter class

---
+++ tools/ft-generator/src/trafficmeter.cpp
@@ -0,0 +1,138 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Traffic meter
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "trafficmeter.h"
+
+namespace generator {
+
+constexpr int ETHER_HEADER_LEN = 14;
+
+void TrafficMeter::OpenFlow(uint64_t flowId, const FlowProfile& profile)
+{
+	if (flowId != _records.size()) {
+		// Assume the flowIds go in order, so we can just use a vector
+		throw std::runtime_error("Unexpected flow ID");
+	}
+
+	FlowRecord rec;
+	rec._l3Proto = profile._l3Proto;
+	rec._l4Proto = profile._l4Proto;
+	_records.push_back(rec);
+}
+
+void TrafficMeter::CloseFlow(uint64_t flowId)
+{
+	(void) flowId;
+	// No need to do anything for now...
+}
+
+void TrafficMeter::ExtractPacketParams(
+	const pcpp::Packet& packet, L3Protocol l3Proto, L4Protocol l4Proto,
+	MacAddress& srcMac, IPAddress& srcIp, uint16_t& srcPort,
+	MacAddress& dstMac, IPAddress& dstIp, uint16_t& dstPort)
+{
+	pcpp::EthLayer* ethLayer = static_cast<pcpp::EthLayer*>(packet.getLayerOfType(pcpp::Ethernet));
+	assert(ethLayer != nullptr);
+	srcMac = ethLayer->getSourceMac();
+	dstMac = ethLayer->getDestMac();
+
+	if (l3Proto == L3Protocol::Ipv4) {
+		pcpp::IPv4Layer* ipv4Layer = static_cast<pcpp::IPv4Layer*>(packet.getLayerOfType(pcpp::IPv4));
+		assert(ipv4Layer != nullptr);
+		srcIp = ipv4Layer->getSrcIPv4Address();
+		dstIp = ipv4Layer->getDstIPv4Address();
+	} else if (l3Proto == L3Protocol::Ipv6) {
+		pcpp::IPv6Layer* ipv6Layer = static_cast<pcpp::IPv6Layer*>(packet.getLayerOfType(pcpp::IPv6));
+		assert(ipv6Layer != nullptr);
+		srcIp = ipv6Layer->getSrcIPv6Address();
+		dstIp = ipv6Layer->getDstIPv6Address();
+	}
+
+	if (l4Proto == L4Protocol::Tcp) {
+		pcpp::TcpLayer* tcpLayer = static_cast<pcpp::TcpLayer*>(packet.getLayerOfType(pcpp::TCP));
+		assert(tcpLayer != nullptr);
+		srcPort = tcpLayer->getSrcPort();
+		dstPort = tcpLayer->getDstPort();
+	} else if (l4Proto == L4Protocol::Udp) {
+		pcpp::UdpLayer* udpLayer = static_cast<pcpp::UdpLayer*>(packet.getLayerOfType(pcpp::UDP));
+		assert(udpLayer != nullptr);
+		srcPort = udpLayer->getSrcPort();
+		dstPort = udpLayer->getDstPort();
+	}
+}
+
+void TrafficMeter::RecordPacket(uint64_t flowId, int64_t time, Direction dir, const pcpp::Packet& packet)
+{
+	assert(flowId < _records.size());
+	FlowRecord& rec = _records[flowId];
+
+	if (rec._fwdPkts == 0 && rec._revPkts == 0) {
+		rec._firstTs = time;
+	}
+	rec._lastTs = time;
+
+	assert(packet.getRawPacket()->getRawDataLen() >= ETHER_HEADER_LEN);
+	assert(dir != Direction::Unknown);
+	if (dir == Direction::Forward) {
+		if (rec._fwdPkts == 0 && rec._revPkts == 0) {
+			ExtractPacketParams(packet, rec._l3Proto, rec._l4Proto,
+				                rec._fwdMacAddr, rec._fwdIpAddr, rec._fwdPort,
+				                rec._revMacAddr, rec._revIpAddr, rec._revPort);
+		}
+		rec._fwdPkts++;
+		rec._fwdBytes += packet.getRawPacket()->getRawDataLen() - ETHER_HEADER_LEN;
+
+	} else if (dir == Direction::Reverse) {
+		if (rec._fwdPkts == 0 && rec._revPkts == 0) {
+			ExtractPacketParams(packet, rec._l3Proto, rec._l4Proto,
+				                rec._revMacAddr, rec._revIpAddr, rec._revPort,
+				                rec._fwdMacAddr, rec._fwdIpAddr, rec._fwdPort);
+		}
+		rec._revPkts++;
+		rec._revBytes += packet.getRawPacket()->getRawDataLen() - ETHER_HEADER_LEN;
+	}
+}
+
+void TrafficMeter::WriteReport()
+{
+	std::cout << "==== Generated flows ====" << std::endl;
+	int i = 0;
+	for (const FlowRecord& rec : _records) {
+		std::cout << "Flow " << i << ": ";
+		std::cout << "firstTs=" << rec._firstTs << " ";
+		std::cout << "lastTs=" << rec._lastTs << " ";
+		std::cout << "fwdBytes=" << rec._fwdBytes << " ";
+		std::cout << "fwdPkts=" << rec._fwdPkts << " ";
+		if (rec._l3Proto == L3Protocol::Ipv4) {
+			std::cout << "fwdIP=" << std::get<IPv4Address>(rec._fwdIpAddr).toString() << " ";
+		} else if (rec._l3Proto == L3Protocol::Ipv6) {
+			std::cout << "fwdIP=" << std::get<IPv6Address>(rec._fwdIpAddr).toString() << " ";
+		}
+		if (rec._l4Proto == L4Protocol::Tcp) {
+			std::cout << "fwdPort=TCP:" << rec._fwdPort << " ";
+		} else if (rec._l4Proto == L4Protocol::Udp) {
+			std::cout << "fwdPort=UDP:" << rec._fwdPort << " ";
+		}
+		std::cout << "revBytes=" << rec._revBytes << " ";
+		std::cout << "revPkts=" << rec._revPkts << " ";
+		if (rec._l3Proto == L3Protocol::Ipv4) {
+			std::cout << "revIP=" << std::get<IPv4Address>(rec._revIpAddr).toString() << " ";
+		} else if (rec._l3Proto == L3Protocol::Ipv6) {
+			std::cout << "revIP=" << std::get<IPv6Address>(rec._revIpAddr).toString() << " ";
+		}
+		if (rec._l4Proto == L4Protocol::Tcp) {
+			std::cout << "revPort=TCP:" << rec._revPort << " ";
+		} else if (rec._l4Proto == L4Protocol::Udp) {
+			std::cout << "revPort=UDP:" << rec._revPort << " ";
+		}
+		std::cout << std::endl;
+		i++;
+	}
+}
+
+} // namespace generator
---
+++ tools/ft-generator/src/trafficmeter.h
@@ -0,0 +1,115 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Traffic meter
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include "flowprofile.h"
+#include "logger.h"
+#include "packet.h"
+
+#include <pcapplusplus/EthLayer.h>
+#include <pcapplusplus/IpAddress.h>
+#include <pcapplusplus/IPv4Layer.h>
+#include <pcapplusplus/IPv6Layer.h>
+#include <pcapplusplus/Layer.h>
+#include <pcapplusplus/Packet.h>
+#include <pcapplusplus/ProtocolType.h>
+#include <pcapplusplus/TcpLayer.h>
+#include <pcapplusplus/UdpLayer.h>
+
+#include <variant>
+#include <vector>
+
+namespace generator {
+
+using MacAddress = pcpp::MacAddress;
+using IPv4Address = pcpp::IPv4Address;
+using IPv6Address = pcpp::IPv6Address;
+using IPAddress = std::variant<std::monostate, IPv4Address, IPv6Address>;
+
+/**
+ * @brief The flow record
+ *
+ */
+struct FlowRecord {
+	L3Protocol _l3Proto;
+	L4Protocol _l4Proto;
+
+	uint64_t _firstTs = 0;
+	uint64_t _lastTs = 0;
+
+	MacAddress _fwdMacAddr;
+	IPAddress _fwdIpAddr;
+	uint64_t _fwdPkts = 0;
+	uint64_t _fwdBytes = 0;
+	uint16_t _fwdPort = 0;
+
+	MacAddress _revMacAddr;
+	IPAddress _revIpAddr;
+	uint64_t _revPkts = 0;
+	uint64_t _revBytes = 0;
+	uint16_t _revPort = 0;
+};
+
+/**
+ * @brief Traffic metering component
+ *
+ * Records packets generated for the flows and keeps statistics about them to provide
+ * a summary of the generated traffic
+ */
+class TrafficMeter {
+public:
+	/**
+	 * @brief Open a new flow for metering
+	 *
+	 * For implementation effectivity reasons, flow ID is currently required to
+	 * be a number starting at 0 with the first call of this method, and
+	 * increasing by 1 by each subsequential call!
+	 *
+	 * @param flowId  The ID of the flow
+	 * @param profile The flow profile
+	 *
+	 * @throws std::runtime_error  When an invalid flow ID is provided
+	 */
+	void OpenFlow(uint64_t flowId, const FlowProfile& profile);
+
+	/**
+	 * @brief Close a flow
+	 *
+	 * @param flowId  The ID of the flow
+	 */
+	void CloseFlow(uint64_t flowId);
+
+	/**
+	 * @brief Update the flow record
+	 *
+	 * @param flowId  The ID of the flow
+	 * @param time    The timestamp of the packet
+	 * @param dir     The direction of the packet
+	 * @param packet  The generated packet
+	 */
+	void RecordPacket(uint64_t flowId, int64_t time, Direction dir, const pcpp::Packet& packet);
+
+	/**
+	 * @brief Write out summary of the recorded flows and packets
+	 *
+	 */
+	void WriteReport();
+
+private:
+	std::vector<FlowRecord> _records;  //< The flow records
+
+	std::shared_ptr<spdlog::logger> _logger = ft::LoggerGet("TrafficMeter");
+
+	void ExtractPacketParams(
+		const pcpp::Packet& packet, L3Protocol l3Proto, L4Protocol l4Proto,
+		MacAddress& srcMac, IPAddress& srcIp, uint16_t& srcPort,
+		MacAddress& dstMac, IPAddress& dstIp, uint16_t& dstPort);
+};
+
+} // namespace generator
