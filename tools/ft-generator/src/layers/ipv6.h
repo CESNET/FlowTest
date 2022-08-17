418f79df59a0835ca95926a1bf9197d08dd094b1
Michal Sedlak <sedlakm@cesnet.cz>
2022-08-17 14:02:41 +0200

    4720 - Add ipv6 layer

---
+++ tools/ft-generator/src/layers/ipv6.h
@@ -0,0 +1,46 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Ipv4 layer planner
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include "../flow.h"
+#include "../layer.h"
+#include "../packet.h"
+
+#include <pcapplusplus/IpAddress.h>
+
+namespace generator {
+
+/**
+ * @brief A representation of a IPv6 layer.
+ */
+class IPv6 : public Layer {
+public:
+
+	/**
+	 * @brief Construct a new IPv6 object
+	 *
+	 * @param ipSrc Source IP address
+	 * @param ipDst Destination IP address
+	 */
+	IPv6(IPv6Address ipSrc, IPv6Address ipDst);
+
+	void PlanFlow(Flow& flow) override;
+
+	void Build(pcpp::Packet& packet, Packet::layerParams& params, Packet& plan) override;
+
+private:
+	IPv6Address _ipSrc;
+	IPv6Address _ipDst;
+	uint8_t _ttl;
+	uint8_t _fwdFlowLabel[3];
+	uint8_t _revFlowLabel[3];
+};
+
+} // namespace generator
