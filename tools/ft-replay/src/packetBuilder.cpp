49af52fcafec584341344f8a1dd802aaab9d5bd2
xsiska12 <xsiska12@stud.fit.vutbr.cz>
2022-09-05 23:58:07 +0000

    4722 - ft-replay - introduce packetBuilder

---
+++ tools/ft-replay/src/packetBuilder.cpp
@@ -0,0 +1,117 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @brief Packet Builder implementation
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "packetBuilder.hpp"
+
+#include <memory>
+#include <cstring>
+
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
+#include <netinet/ip.h>
+#include <netinet/ip6.h>
+
+namespace replay {
+
+struct VlanHeader {
+	uint16_t vlanTci;
+  	uint16_t etherType;
+} __attribute__((packed));
+
+void PacketBuilder::SetVlan(uint16_t vlanID)
+{
+	_vlanID = vlanID;
+}
+
+void PacketBuilder::SetTimeMultiplier(float timeMultiplier)
+{
+	_timeMultiplier = timeMultiplier;
+}
+
+std::unique_ptr<Packet> PacketBuilder::Build(const RawPacket *rawPacket)
+{
+	Packet packet;
+	packet.timestamp = GetMultipliedTimestamp(rawPacket->timestamp);
+	packet.dataLen = rawPacket->dataLen;
+	packet.info = GetPacketL3Info(rawPacket);
+	if (_vlanID) {
+		packet.info.l3Offset += sizeof(VlanHeader);
+		packet.dataLen += sizeof(VlanHeader);
+		packet.data = GetDataCopyWithVlan(rawPacket->data, rawPacket->dataLen);
+	} else {
+		packet.data = GetDataCopy(rawPacket->data, rawPacket->dataLen);
+	}
+	return std::make_unique<Packet>(std::move(packet));
+}
+
+uint64_t PacketBuilder::GetMultipliedTimestamp(uint64_t rawPacketTimestamp) const
+{
+	return rawPacketTimestamp * _timeMultiplier;
+}
+
+PacketInfo PacketBuilder::GetPacketL3Info(const RawPacket *rawPacket) const
+{
+	CheckSufficientDataLength(rawPacket->dataLen, sizeof(ethhdr));
+
+	PacketInfo info;
+	const ethhdr *ethHeader = reinterpret_cast<const ethhdr *>(rawPacket->data);
+	switch (ntohs(ethHeader->h_proto)) {
+	case ETH_P_IP:
+		CheckSufficientDataLength(rawPacket->dataLen, sizeof(ethhdr) + sizeof(iphdr));
+		info.l3Type = L3Type::IPv4;
+		break;
+	case ETH_P_IPV6:
+		CheckSufficientDataLength(rawPacket->dataLen, sizeof(ethhdr) + sizeof(ip6_hdr));
+		info.l3Type = L3Type::IPv6;
+		break;
+	default:
+		_logger->error("Cannot parse L3 header, unsuported protocol");
+		throw std::runtime_error("PacketBuilder::GetPacketL3Info() has failed");
+	}
+	info.l3Offset = sizeof(ethhdr);
+	return info;
+}
+
+void PacketBuilder::CheckSufficientDataLength(size_t availableLength, size_t requiredLength) const
+{
+	if (availableLength < requiredLength) {
+		_logger->error("Unsufficient packet data length");
+		throw std::runtime_error("PacketBuilder::CheckSufficientDataLength() has failed");
+	}
+}
+
+std::unique_ptr<std::byte[]> PacketBuilder::GetDataCopy(const std::byte *rawData, uint16_t dataLen)
+{
+	std::unique_ptr<std::byte[]> packetData;
+	packetData = std::unique_ptr<std::byte[]>(new std::byte[dataLen]);
+	std::memcpy(packetData.get(), rawData, dataLen);
+	return packetData;
+}
+
+std::unique_ptr<std::byte[]> PacketBuilder::GetDataCopyWithVlan(const std::byte *rawData, uint16_t dataLen)
+{
+	std::unique_ptr<std::byte[]> packetData;
+	packetData = std::unique_ptr<std::byte[]>(new std::byte[dataLen + sizeof(VlanHeader)]);
+	std::memcpy(packetData.get(), rawData, sizeof(ethhdr));
+	std::memcpy(packetData.get() + sizeof(ethhdr) + sizeof(VlanHeader),
+		rawData + sizeof(ethhdr), dataLen - sizeof(ethhdr));
+
+	constexpr uint16_t vlanEtherType = 0x8100;
+
+	ethhdr *ethHeader = reinterpret_cast<ethhdr *>(packetData.get());
+	uint16_t originL2EtherType = ethHeader->h_proto;
+	ethHeader->h_proto = vlanEtherType;
+
+	VlanHeader *vlanHeader = reinterpret_cast<VlanHeader *>(packetData.get() + sizeof(ethhdr));
+	vlanHeader->vlanTci = htons(_vlanID);
+	vlanHeader->etherType = originL2EtherType;
+
+	return packetData;
+}
+
+} // namespace replay
---
+++ tools/ft-replay/src/packetBuilder.hpp
@@ -0,0 +1,64 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @brief Packet Builder interface
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include "packet.hpp"
+#include "rawPacketProvider.hpp"
+
+#include <memory>
+
+namespace replay {
+
+/**
+ * @brief Packet class builder
+ */
+class PacketBuilder {
+public:
+
+	/**
+	 * @brief Build Packet structure from RawPacket structure
+	 *
+	 * @param rawPacket RawPacket information
+	 * @return std::unique_ptr<Packet> Builded packet
+	 */
+	std::unique_ptr<Packet> Build(const RawPacket *rawPacket);
+
+	/**
+	 * @brief Set the Vlan ID value
+	 *
+	 * If Vlan ID is set then vlan header is inserted into the packet
+	 * Default: 0 - do not insert vlan header
+	 *
+	 * @param vlanID Vlan ID to set.
+	 */
+	void SetVlan(uint16_t vlanID);
+
+	/**
+	 * @brief Set the time multiplier value
+	 *
+	 * Timestamp in packet are multiplied by this value
+	 * Default: 0
+	 *
+	 * @param timeMultiplier Value to set
+	 */
+	void SetTimeMultiplier(float timeMultiplier);
+
+private:
+	uint64_t GetMultipliedTimestamp(uint64_t rawPacketTimestamp) const;
+	PacketInfo GetPacketL3Info(const RawPacket *rawPacket) const;
+	void CheckSufficientDataLength(size_t availableLength, size_t requiredLength) const;
+	std::unique_ptr<std::byte[]> GetDataCopy(const std::byte *rawData, uint16_t dataLen);
+	std::unique_ptr<std::byte[]> GetDataCopyWithVlan(const std::byte *rawData, uint16_t dataLen);
+
+	uint16_t _vlanID = 0;
+	float _timeMultiplier = 0;
+	std::shared_ptr<spdlog::logger> _logger = ft::LoggerGet("PacketBuilder");
+};
+
+} // namespace replay
