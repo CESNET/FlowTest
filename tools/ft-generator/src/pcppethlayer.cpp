96c568b382e988fc5ffa0c446df87f147bfa683d
Michal Sedlak <sedlakm@cesnet.cz>
2022-11-11 16:17:03 +0100

    Generator - add pcpp::EthLayer wrapper to customize computeCalculateFields behavior

---
+++ tools/ft-generator/src/pcppethlayer.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief A wrapper class around pcpp::EthLayer
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pcppethlayer.h"
+
+namespace generator {
+
+void PcppEthLayer::computeCalculateFields()
+{
+	// do nothing
+}
+
+} // namespace generator
---
+++ tools/ft-generator/src/pcppethlayer.h
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief A wrapper class around pcpp::EthLayer
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include <pcapplusplus/EthLayer.h>
+
+namespace generator {
+
+/**
+ * @brief A customized pcpp::EthLayer
+ *
+ * This is needed because the original pcpp::EthLayer's computeCalculateFields
+ * overwrites the etherType, yet only supports a very limited set of layers, so
+ * we set the correct etherType ourselves instead of relying on the
+ * computeCalculateFields method to do so automatically.
+ */
+class PcppEthLayer : public pcpp::EthLayer {
+public:
+	using pcpp::EthLayer::EthLayer;
+
+	/**
+	 * @brief Override computeCalculateFields of pcpp::EthLayer to do nothing
+	 */
+	void computeCalculateFields() override;
+};
+
+} // generator
