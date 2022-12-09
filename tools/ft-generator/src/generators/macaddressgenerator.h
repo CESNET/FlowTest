a0365c37fbdf16babbc4f74dc80ce755078ccc7d
Michal Sedlak <sedlakm@cesnet.cz>
2022-12-09 12:38:27 +0100

    Generator - introduce mac address generator

---
+++ tools/ft-generator/src/generators/macaddressgenerator.h
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Mac address generator
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "prefixedgenerator.h"
+
+#include <pcapplusplus/MacAddress.h>
+
+namespace generator {
+
+using MacAddress = pcpp::MacAddress;
+
+/**
+ * @brief Generator of Mac addresses
+ */
+class MacAddressGenerator {
+public:
+	/**
+	 * @brief Construct a new Mac Address Generator object
+	 *
+	 * @param baseAddr   The base address to generate the addresses from, respectively the prefix
+	 * @param prefixLen  The prefix length
+	 * @param seed       The seed
+	 */
+	MacAddressGenerator(MacAddress baseAddr, uint8_t prefixLen, const std::bitset<128>& seed);
+
+	/**
+	 * @brief Generate a Mac address
+	 *
+	 * @return MacAddress
+	 */
+	MacAddress Generate();
+
+private:
+	PrefixedGenerator _gen;
+};
+
+} // namespace generator
