f2ce2a29c1b8fc764c9d6bdfe80c80342e08d4c1
Michal Sedlak <sedlakm@cesnet.cz>
2023-02-09 19:35:55 +0100

    Generator - improve usage message

--- tools/ft-generator/src/config/commandlineargs.cpp
+++ tools/ft-generator/src/config/commandlineargs.cpp
@@ -70,16 +70,16 @@ void CommandLineArgs::Parse(int argc, char** argv)
 
 void CommandLineArgs::PrintUsage()
 {
-	std::cerr << "Usage: ./ft-generator [-v] -p <flow profile file> -o <output pcap file> -r "
-				 "<output report file>\n";
-	std::cerr << "  --profiles, -p  ... The flow profiles file in csv format\n";
-	std::cerr << "  --output, -o    ... The output pcap file\n";
-	std::cerr << "  --config, -c    ... The yaml config file\n";
+	std::cerr << "Usage: ./ft-generator [-v] -p <flow profile file> -c <config file> -o <output "
+				 "pcap file> -r <output report file>\n";
+	std::cerr << "  --profiles, -p FILE  The flow profiles file in csv format\n";
+	std::cerr << "  --output, -o FILE    The output pcap file\n";
+	std::cerr << "  --config, -c FILE    The yaml config file\n";
 	std::cerr
-		<< "  --report, -r    ... The output csv file with the report of the generated flows\n";
-	std::cerr << "  --verbose, -v   ... Verbosity level, specify multiple times for more verbose "
+		<< "  --report, -r FILE    The output csv file with the report of the generated flows\n";
+	std::cerr << "  --verbose, -v        Verbosity level, specify multiple times for more verbose "
 				 "logging\n";
-	std::cerr << "  --help, -h      ... Show this help message\n";
+	std::cerr << "  --help, -h           Show this help message\n";
 }
 
 void CommandLineArgs::CheckValidity()
