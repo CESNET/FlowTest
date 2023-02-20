0ad69085557631128a22930e48faaf6b389b355f
Jan Sobol <sobol@cesnet.cz>
2023-02-20 15:10:12 +0100

    7056 - ft-orchestration - allow ssh agent in static configuration Check only password (not) filled. Username can be used with password, key_path and ssh_agent.

--- tools/ft-orchestration/src/config/authentication.py
+++ tools/ft-orchestration/src/config/authentication.py
@@ -28,19 +28,23 @@ class AuthenticationCfg(YAMLWizard):
     key_path: Optional[str] = None
     username: Optional[str] = None
     password: Optional[str] = None
+    ssh_agent: bool = False
 
     def check(self) -> None:
         """Check the configuration validity."""
-        if self.key_path and self.password and self.username:
+        if self.key_path and self.password:
             raise AuthenticationCfgException(
-                "AuthenticationCfg config file can not contain both of the key_path and the username/password."
+                "AuthenticationCfg config file can not contain both of the key_path and the password."
             )
+        if self.ssh_agent and (self.key_path or self.password):
+            raise AuthenticationCfgException("Key path or password cannot be set if the ssh agent is used.")
+        if not (self.ssh_agent or self.key_path or self.password):
+            raise AuthenticationCfgException(
+                "At least one authentication (SSH agent, key_path or password) must be present."
+            )
+
         if self.key_path:
             self._check_key_path()
-        elif self.password and self.username:
-            pass
-        else:
-            raise AuthenticationCfgException("Field key_path or both fields username and password must be present.")
 
     def _check_key_path(self) -> None:
         if not exists(self.key_path):
