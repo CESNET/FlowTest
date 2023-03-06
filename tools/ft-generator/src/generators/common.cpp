723b1dc0f62fbae4b21a991382cd09d0a22a1e21
Michal Sedlak <sedlakm@cesnet.cz>
2023-03-06 02:36:32 +0100

    Generator - fix addresses out of range being generated because of bad bit order

--- tools/ft-generator/src/generators/common.cpp
+++ tools/ft-generator/src/generators/common.cpp
@@ -10,10 +10,21 @@
 
 namespace generator {
 
+static uint8_t ReverseBits(uint8_t b)
+{
+	// Reverse the bit order in a byte
+	b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+	b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+	b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+	return b;
+}
+
 void BitsetToBytes(std::bitset<128> bitset, uint8_t* bytes, int length)
 {
 	for (int i = 0; i < length; i++) {
-		bytes[i] = (bitset & std::bitset<128>(0xFF)).to_ulong();
+		uint8_t b = (bitset & std::bitset<128>(0xFF)).to_ulong();
+		b = ReverseBits(b);
+		bytes[i] = b;
 		bitset >>= 8;
 	}
 }
@@ -22,7 +33,9 @@ std::bitset<128> BytesToBitset(const uint8_t* bytes, int length)
 {
 	std::bitset<128> bitset {0};
 	for (int i = 0; i < length; i++) {
-		bitset = (bitset << 8) | std::bitset<128>(bytes[length - 1 - i]);
+		uint8_t b = bytes[length - 1 - i];
+		b = ReverseBits(b);
+		bitset = (bitset << 8) | std::bitset<128>(b);
 	}
 	return bitset;
 }
