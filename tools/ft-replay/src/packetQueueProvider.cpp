62afb4f6f52f6e3db8daf7b47e4a2c759b4c9931
Pavel Siska <siska@cesnet.cz>
2023-04-25 11:37:43 +0000

    ft-replay - add function GetPacketQueueRatioById Function GetPacketQueueRatioById() retrieves the percentage share of the total packets and bytes in the specified queue

--- tools/ft-replay/src/packetQueueProvider.cpp
+++ tools/ft-replay/src/packetQueueProvider.cpp
@@ -10,12 +10,14 @@
 
 #include <cassert>
 #include <memory>
+#include <numeric>
 
 namespace replay {
 
 PacketQueueProvider::PacketQueueProvider(size_t queueCount)
 	: _queueCount(queueCount)
 {
+	_queuesDistribution.resize(_queueCount, {0, 0});
 	for (size_t i = 0; i < _queueCount; i++) {
 		_packetQueues.emplace_back(std::make_unique<PacketQueue>());
 	}
@@ -24,12 +26,49 @@ PacketQueueProvider::PacketQueueProvider(size_t queueCount)
 void PacketQueueProvider::InsertPacket(std::unique_ptr<Packet> packet)
 {
 	uint8_t queueID = GetPacketQueueId(*packet);
+	UpdateQueueDistribution(_queuesDistribution[queueID], packet->dataLen);
 	_packetQueues[queueID]->emplace_back(std::move(packet));
+}
+
+void PacketQueueProvider::UpdateQueueDistribution(
+	PacketQueueProvider::QueueDistribution& queueDistribution,
+	size_t packetLength)
+{
+	queueDistribution.packets++;
+	queueDistribution.bytes += packetLength;
 }
 
 uint8_t PacketQueueProvider::GetPacketQueueId(const Packet& packet)
 {
 	return packet.GetHash() % _queueCount;
+}
+
+PacketQueueProvider::QueueDistribution
+PacketQueueProvider::GetPacketQueueRatioById(uint8_t queueId) const
+{
+	assert(queueId < _queueCount);
+
+	auto queueDistributionSum = [](QueueDistribution sum, const QueueDistribution& distribution) {
+		sum.packets += distribution.packets;
+		sum.bytes += distribution.bytes;
+		return sum;
+	};
+
+	auto queueDistributionTotal = std::accumulate(
+		_queuesDistribution.begin(),
+		_queuesDistribution.end(),
+		QueueDistribution(),
+		queueDistributionSum);
+
+	QueueDistribution queueDistribution = _queuesDistribution[queueId];
+	if (queueDistributionTotal.packets) {
+		queueDistribution.packets /= queueDistributionTotal.packets;
+	}
+	if (queueDistributionTotal.bytes) {
+		queueDistribution.bytes /= queueDistributionTotal.bytes;
+	}
+
+	return queueDistribution;
 }
 
 const std::unique_ptr<PacketQueue> PacketQueueProvider::GetPacketQueueById(uint8_t queueId)
--- tools/ft-replay/src/packetQueueProvider.hpp
+++ tools/ft-replay/src/packetQueueProvider.hpp
@@ -11,6 +11,7 @@
 #include "packet.hpp"
 
 #include <memory>
+#include <utility>
 #include <vector>
 
 namespace replay {
@@ -47,9 +48,34 @@ public:
 	 */
 	const std::unique_ptr<PacketQueue> GetPacketQueueById(uint8_t queueId);
 
+	/**
+	 * @brief Represents the percentage share of the total packets and bytes in a packet queue
+	 *
+	 * Calculated against the total number of packets and bytes in all queues.
+	 */
+	struct QueueDistribution {
+		double packets; /**< The percentage share of the packets in the queue */
+		double bytes; /**< The percentage share of the bytes in the queue */
+	};
+
+	/**
+	 * @brief Retrieves the percentage share of the total packets and bytes in the specified queue
+	 *
+	 * This function returns a pair of doubles representing the percentage share of the total
+	 * packets and bytes in the queue with the given queue ID, as calculated against the total
+	 * number of packets and bytes in all queues.
+	 *
+	 * @param queueId The ID of the packet queue for which to calculate the percentage share.
+	 * @return A pair of doubles representing the percentage share of packets and bytes in the
+	 * specified queue.
+	 */
+	QueueDistribution GetPacketQueueRatioById(uint8_t queueId) const;
+
 private:
 	uint8_t GetPacketQueueId(const Packet& packet);
+	void UpdateQueueDistribution(QueueDistribution& queueDistribution, size_t packetLength);
 
+	std::vector<QueueDistribution> _queuesDistribution;
 	std::vector<std::unique_ptr<PacketQueue>> _packetQueues;
 	size_t _queueCount;
 };
