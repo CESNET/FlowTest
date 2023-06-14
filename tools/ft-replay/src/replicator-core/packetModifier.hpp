82f4a2c9b6153f0535832d211cc4e8e2094440ab
Pavel Siska <siska@cesnet.cz>
2023-06-14 13:40:05 +0200

    ft-replay - PacketModifier - add checker whether modifier is enabled

--- tools/ft-replay/src/replicator-core/packetModifier.hpp
+++ tools/ft-replay/src/replicator-core/packetModifier.hpp
@@ -26,6 +26,16 @@ public:
 	PacketModifier(ModifierStrategies modifierStrategies);
 
 	/**
+	 * @brief Checks if a packet modifier is in specific loop enabled.
+	 *
+	 * This function checks whether a loop with the given ID is enabled or not.
+	 *
+	 * @param loopId The ID of the loop to check.
+	 * @return True if the loop is enabled, false otherwise.
+	 */
+	bool IsEnabledThisLoop(size_t loopId) noexcept;
+
+	/**
 	 * @brief Apply packet modifier strategies to given packet
 	 *
 	 * @param ptr Pointer to the beggining of packet data
