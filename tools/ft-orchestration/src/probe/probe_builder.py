e3de7b2a634e26fa4e7fb3a56c3d41c6d6d820c9
Tomas Jansky <Tomas.Jansky@progress.com>
2023-06-25 21:06:29 +0200

    FT-Orchestration - Allow to Create Probe with Custom init arguments

--- tools/ft-orchestration/src/probe/probe_builder.py
+++ tools/ft-orchestration/src/probe/probe_builder.py
@@ -79,13 +79,15 @@ class ProbeBuilder(BuilderBase, Device):
         self._class = self._find_class(PROBE_IMPORT_PATH, probe_cfg.type, ProbeInterface)
 
     # pylint: disable=arguments-differ
-    def get(self, required_protocols: List[str]) -> ProbeInterface:
+    def get(self, protocols: list[str], **kwargs) -> ProbeInterface:
         """Create probe instance from static configuration by alias identifier.
 
         Parameters
         ----------
-        required_protocols : List[str]
+        protocols : list[str]
             List of the networking protocols which the probe should parse and export.
+        kwargs: dict
+            Custom arguments passed to the init function of the probe
 
         Returns
         -------
@@ -93,7 +95,7 @@ class ProbeBuilder(BuilderBase, Device):
             New probe instance.
         """
 
-        return self._class(self._host, self._target, required_protocols, self._interfaces, **self._connector_args)
+        return self._class(self._host, self._target, protocols, self._interfaces, **{**self._connector_args, **kwargs})
 
     def get_mac_addresses(self) -> List[str]:
         """Get list of hardware addresses of enabled interfaces.
