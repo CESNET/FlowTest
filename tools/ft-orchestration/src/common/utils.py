6ff3cbd23668726b5aaf361c8a6e12b6b9f5aa10
Tomas Jansky <Tomas.Jansky@progress.com>
2023-06-25 20:40:51 +0200

    FT-Orchestration - Implemented download_logs

--- tools/ft-orchestration/src/common/utils.py
+++ tools/ft-orchestration/src/common/utils.py
@@ -20,3 +20,24 @@ def get_project_root() -> str:
     """
 
     return os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../../../"))
+
+
+def download_logs(dest: str, **kwargs) -> None:
+    """Download logs from test components.
+
+    Parameters
+    ----------
+    dest: str
+        Path to a directory where logs should be downloaded to.
+    *kwargs: Any
+        Objects which logs should be downloaded.
+        The argument name will be used to create the directory.
+    """
+
+    for name, device in kwargs.items():
+        if device is None:
+            continue
+
+        logs_dir = os.path.join(dest, name)
+        os.makedirs(logs_dir, exist_ok=True)
+        device.download_logs(logs_dir)
