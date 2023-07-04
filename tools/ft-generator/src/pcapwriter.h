b7db2f1142f2acf22950358a385219b82677e6fc
Michal Sedlak <sedlakm@cesnet.cz>
2023-07-04 10:18:31 +0200

    ft-generator - check for write errors in PcapWriter

--- tools/ft-generator/src/pcapwriter.h
+++ tools/ft-generator/src/pcapwriter.h
@@ -11,6 +11,7 @@
 #include <pcap/pcap.h>
 
 #include <cstddef>
+#include <cstdio>
 #include <memory>
 #include <string>
 
@@ -30,18 +31,27 @@ public:
 	 */
 	explicit PcapWriter(const std::string& filename);
 
+	// Disable copy and move constructors
+	PcapWriter(const PcapWriter&) = delete;
+	PcapWriter(PcapWriter&&) = delete;
+	PcapWriter& operator=(const PcapWriter&) = delete;
+	PcapWriter& operator=(PcapWriter&&) = delete;
+
 	/**
 	 * @brief Write a packet to the output file
 	 *
 	 * @param data  The packet data
 	 * @param length  The packet length
 	 * @param timestamp  The packet timestamp
+	 *
+	 * @throws runtime_error  When file write failed
 	 */
 	void WritePacket(const std::byte* data, uint32_t length, timeval timestamp);
 
 private:
 	std::unique_ptr<pcap_t, decltype(&pcap_close)> _pcap {nullptr, pcap_close};
 	std::unique_ptr<pcap_dumper_t, decltype(&pcap_dump_close)> _dumper {nullptr, pcap_dump_close};
+	std::FILE* _fp = 0;
 };
 
 } // namespace generator
