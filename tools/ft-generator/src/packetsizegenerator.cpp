f03da194ebb57ad17a0a2fa6a32d97127b2ade35
Michal Sedlak <sedlakm@cesnet.cz>
2023-07-18 07:08:13 +0200

    ft-generator - integrate the new PacketSizeGenerator and choose between the two variants to yield optimal results

--- tools/ft-generator/src/packetsizegenerator.cpp
+++ tools/ft-generator/src/packetsizegenerator.cpp
@@ -7,16 +7,35 @@
  */
 
 #include "packetsizegenerator.h"
+#include "packetsizegeneratorfast.h"
 #include "packetsizegeneratorslow.h"
 
 namespace generator {
+
+/**
+ * The packet count threshold for deciding between the different variants of generators.
+ * Arbitratily chosen. Can be adjusted later.
+ */
+static constexpr int SLOW_GENERATOR_PKT_COUNT_THRESHOLD = 50;
 
 std::unique_ptr<PacketSizeGenerator> PacketSizeGenerator::Construct(
 	const std::vector<IntervalInfo>& intervals,
 	uint64_t numPkts,
 	uint64_t numBytes)
 {
-	return std::make_unique<PacketSizeGeneratorSlow>(intervals, numPkts, numBytes);
+	/**
+	 * The "Slow" generator produces better results for smaller flows, but is
+	 * expensive to compute. Using this generator for large flows is very slow!
+	 *
+	 * The "Fast" generator produces is ideal for flows with larger amounts of
+	 * packets, as it produces even better results than the "Slow" variant
+	 * given enough packets, and is much faster.
+	 */
+	if (numPkts < SLOW_GENERATOR_PKT_COUNT_THRESHOLD) {
+		return std::make_unique<PacketSizeGeneratorSlow>(intervals, numPkts, numBytes);
+	} else {
+		return std::make_unique<PacketSizeGeneratorFast>(intervals, numPkts, numBytes);
+	}
 }
 
 } // namespace generator
