49fc4ba0d3e35d571733b4e21c0c3f2debd158f8
Michal Sedlak <sedlakm@cesnet.cz>
2023-07-24 11:51:27 +0200

    ft-generator - integrate doctest

---
+++ tools/ft-generator/src/tests/main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Main entry point for doctest test runner
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
