CESNET/FlowTest

commit b54b47409af910d3c40bb2ff4d1b8abc52cc33e0

prepare_machine - use localhost inventory to run any ansible playbook

prepare_machine.sh
@@ -86,6 +86,7 @@ ansible-playbook \
 	${verbosity} \
 	-b --become-user "${become}" ${ask_become_pass} \
 	-e ansible_remote_tmp="${tmp_dir}" \
+	-i localhost, \
 	"${playbook}"
 
 ansible_rc=$?

ansible/dev_machine.yaml
@@ -1,6 +1,5 @@
 ---
-- hosts: localhost
-  connection: local
+- hosts: all
   become: yes
   roles:
     - role: python_upgrade

commit 01e62617a22274330dc2b8515ae5b4b16cbb34b3

prepare_machine - fix missing enforcement of local connection
Fixes: b54b47409a (prepare_machine - use localhost inventory...)

prepare_machine.sh
@@ -87,6 +87,7 @@ ansible-playbook \
 	-b --become-user "${become}" ${ask_become_pass} \
 	-e ansible_remote_tmp="${tmp_dir}" \
 	-i localhost, \
+	--connection=local \
 	"${playbook}"
 
 ansible_rc=$?