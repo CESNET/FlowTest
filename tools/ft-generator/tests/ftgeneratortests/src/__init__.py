b8cf5f2e821ca601e25f898588d59051df38768e
Matej Hulák <hulakmat@gmail.com>
2023-09-07 09:20:17 +0200

    ft-generator-tests - Added src.__init__

---
+++ tools/ft-generator/tests/ftgeneratortests/src/__init__.py
@@ -0,0 +1,31 @@
+"""
+Author(s): Matej Hulák <hulak@cesnet.cz>
+
+Copyright: (C) 2023 CESNET, z.s.p.o.
+SPDX-License-Identifier: BSD-3-Clause
+
+isort:skip_file
+"""
+
+from ftgeneratortests.src.constants import (
+    GENERATOR_ERROR_RET_CODE,
+    L3_PROTOCOL_IPV4,
+    L3_PROTOCOL_IPV6,
+    L4_PROTOCOL_TCP,
+    L4_PROTOCOL_UDP,
+    L4_PROTOCOL_ICMP,
+    L4_PROTOCOL_ICMPV6,
+    L4_PROTOCOL_NONE,
+    MAC_MASK_SIZE,
+    MTU_SIZE,
+    ENCAPSULATION_ABS_TOLERANCE,
+    FRAGMENTATION_ABS_TOLERANCE,
+    REPORT_REL_TOLERANCE,
+    TIMING_REL_TOLERANCE,
+)
+from ftgeneratortests.src.common import get_prob_from_str
+from ftgeneratortests.src.flow import ExtendedFlow, Flow
+from ftgeneratortests.src.cache import FlowCache
+from ftgeneratortests.src.generator_config import GeneratorConfig
+from ftgeneratortests.src.generator import Generator
+from ftgeneratortests.src.parser import parse_pcap, parse_profiles, parse_report
