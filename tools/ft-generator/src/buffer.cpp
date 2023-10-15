38ac31a02328936b2fb79bd2c06faa753c6fe47a
Michal Sedlak <sedlakm@cesnet.cz>
2023-10-15 06:14:22 +0200

    ft-generator - improve Buffer class

--- tools/ft-generator/src/buffer.cpp
+++ tools/ft-generator/src/buffer.cpp
@@ -15,21 +15,35 @@
 
 namespace generator {
 
-void Buffer::Write(const char* data, std::size_t len, std::size_t pos)
+void Buffer::WriteAt(std::size_t pos, const std::byte* data, std::size_t len)
 {
-	if (pos > _data.size()) {
-		throw std::logic_error("buffer position is out of bounds");
+	if (pos + len > _data.size()) {
+		throw std::logic_error("write would happen out of buffer bounds");
 	}
 
-	if (pos + len > _data.size()) {
-		_data.resize(pos + len);
-	}
 	std::memcpy(_data.data() + pos, data, len);
+}
+
+void Buffer::WriteAt(std::size_t pos, const char* data, std::size_t len)
+{
+	WriteAt(pos, reinterpret_cast<const std::byte*>(data), len);
+}
+
+const std::byte* Buffer::PtrAt(std::size_t pos) const
+{
+	return _data.data() + pos;
+}
+
+void Buffer::Append(const std::byte* data, std::size_t len)
+{
+	std::size_t end = _data.size();
+	_data.resize(end + len);
+	WriteAt(end, data, len);
 }
 
 void Buffer::Append(const char* data, std::size_t len)
 {
-	Write(data, len, _data.size());
+	Append(reinterpret_cast<const std::byte*>(data), len);
 }
 
 std::unique_ptr<pcpp::Layer> Buffer::ToLayer() const
@@ -38,6 +52,16 @@ std::unique_ptr<pcpp::Layer> Buffer::ToLayer() const
 		reinterpret_cast<const uint8_t*>(_data.data()),
 		_data.size(),
 		true);
+}
+
+void Buffer::WriteAt(std::size_t pos, std::string_view value)
+{
+	WriteAt(pos, reinterpret_cast<const std::byte*>(value.data()), value.size());
+}
+
+void Buffer::Append(std::string_view value)
+{
+	Append(reinterpret_cast<const std::byte*>(value.data()), value.size());
 }
 
 } // namespace generator
