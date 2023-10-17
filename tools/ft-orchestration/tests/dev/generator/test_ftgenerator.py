f72a5ae6ccd9b240107563669110662a24f7c216
Jan Sobol <sobol@cesnet.cz>
2023-10-17 16:56:43 +0200

    ft-orchestration - update ft_generator tests (for use Executable)

--- tools/ft-orchestration/tests/dev/generator/test_ftgenerator.py
+++ tools/ft-orchestration/tests/dev/generator/test_ftgenerator.py
@@ -10,16 +10,15 @@ Tests for ft-generator connector.
 import filecmp
 import os
 import re
+import shlex
 import shutil
 import subprocess
 import tempfile
 from pathlib import Path
 
 import pytest
+from lbr_testsuite.executable import Executor, RemoteExecutor, Rsync, Tool
 from src.generator.ft_generator import FtGenerator, FtGeneratorConfig
-from src.host.common import get_real_user
-from src.host.host import Host
-from src.host.storage import RemoteStorage
 
 HOST = os.environ.get("PYTEST_TEST_HOST")
 USERNAME = os.environ.get("PYTEST_TEST_HOST_USERNAME")
@@ -28,71 +27,66 @@ INPUT_DIR = os.path.join(FILE_DIR, "input")
 REF_DIR = os.path.join(FILE_DIR, "ref")
 
 
-@pytest.fixture(name="host", scope="function")
-def fixture_host():
+@pytest.fixture(name="executor", scope="function")
+def fixture_executor():
     """Fixture for connecting to remote."""
 
     if not HOST:
         pytest.skip("PYTEST_TEST_HOST env variable must be defined.")
 
     if USERNAME:
-        storage = RemoteStorage(HOST, user=USERNAME)
-        host = Host(HOST, storage, user=USERNAME)
+        executor = RemoteExecutor(HOST, user=USERNAME)
     else:
-        storage = RemoteStorage(HOST)
-        host = Host(HOST, storage)
-    yield host
-    host.get_storage().remove_all()
-    host.get_storage().close()
-    host.close()
+        executor = RemoteExecutor(HOST)
+    yield executor
+    Rsync(executor).wipe_data_directory()
+    executor.close()
 
 
 # pylint: disable=too-few-public-methods
-class HostStub(Host):
-    """Fake host class. It does not run ft-generator, only returns predefined files."""
+class ExecutorStub(RemoteExecutor):
+    """Fake executor class. It does not run ft-generator, only returns predefined files."""
 
     def __init__(
         self,
         res_pcap,
         res_csv,
-        host="localhost",
-        storage=None,
-        user=get_real_user(),
-        password=None,
-        key_filename=None,
+        *args,
+        **kwargs,
     ):
-        super().__init__(host, storage, user, password, key_filename)
+        super().__init__(*args, **kwargs)
         self._res_pcap = res_pcap
         self._res_csv = res_csv
 
-    def run(self, command: str, asynchronous=False, check_rc=True, timeout=None, path_replacement=True):
+    def run(self, cmd, netns=None, sudo=False, **options):
         """Modified run method."""
 
-        if command.startswith("ft-generator"):
-            res = re.search(r"^ft-generator\s+-p\s+\S+\s+-o\s+(\S+)\s+-r\s+(\S+)", command)
+        if isinstance(cmd, list):
+            cmd = shlex.join(cmd)
+
+        if cmd.startswith("ft-generator"):
+            res = re.search(r"^ft-generator\s+-p\s+\S+\s+-o\s+(\S+)\s+-r\s+(\S+)", cmd)
             pcap_path, csv_path = res.groups()
 
-            if self.is_local():
-                shutil.copy(self._res_pcap, pcap_path)
-                shutil.copy(self._res_csv, csv_path)
-            else:
-                self.get_storage().push(self._res_pcap)
-                self.get_storage().push(self._res_csv)
+            rsync = Rsync(self)
 
-                tmp_path = os.path.join(self.get_storage().get_remote_directory(), os.path.basename(self._res_pcap))
-                self.run(f"mv -f {tmp_path} {pcap_path}")
+            rsync.push_path(self._res_pcap)
+            rsync.push_path(self._res_csv)
 
-                tmp_path = os.path.join(self.get_storage().get_remote_directory(), os.path.basename(self._res_csv))
-                self.run(f"mv -f {tmp_path} {csv_path}")
+            tmp_path = os.path.join(rsync.get_data_directory(), os.path.basename(self._res_pcap))
+            Tool(f"mv -f {tmp_path} {pcap_path}", executor=self).run()
+
+            tmp_path = os.path.join(rsync.get_data_directory(), os.path.basename(self._res_csv))
+            Tool(f"mv -f {tmp_path} {csv_path}", executor=self).run()
 
             return None
 
-        return super().run(command, asynchronous, check_rc, timeout, path_replacement)
+        return super().run(cmd, netns, sudo, **options)
 
 
-@pytest.fixture(name="host_stub", scope="function")
-def fixture_host_stub(request: pytest.FixtureRequest):
-    """Fixture for getting fake host object."""
+@pytest.fixture(name="executor_stub", scope="function")
+def fixture_executor_stub(request: pytest.FixtureRequest):
+    """Fixture for getting fake executor object."""
 
     res_pcap, res_csv = request.param
 
@@ -100,21 +94,26 @@ def fixture_host_stub(request: pytest.FixtureRequest):
         pytest.skip("PYTEST_TEST_HOST env variable must be defined.")
 
     if USERNAME:
-        storage = RemoteStorage(HOST, user=USERNAME)
-        host = HostStub(res_pcap, res_csv, HOST, storage, user=USERNAME)
+        executor = ExecutorStub(res_pcap, res_csv, HOST, user=USERNAME)
     else:
-        storage = RemoteStorage(HOST)
-        host = HostStub(res_pcap, res_csv, HOST, storage)
-    yield host
-    host.get_storage().remove_all()
-    host.get_storage().close()
-    host.close()
+        executor = ExecutorStub(res_pcap, res_csv, HOST)
+    yield executor
+    Rsync(executor).wipe_data_directory()
+    executor.close()
 
 
-def test_generate(host: Host):
+def host_file_exist(executor: Executor, filename: str) -> bool:
+    """Check if file exist on host with executor."""
+
+    cmd = Tool(["test", "-f", filename], executor=executor, failure_verbosity="silent")
+    cmd.run()
+    return cmd.returncode() == 0
+
+
+def test_generate(executor: Executor):
     """Test of generate method with basic profile and no configuration."""
 
-    generator = FtGenerator(host)
+    generator = FtGenerator(executor)
     pcap, csv = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
     assert pcap.endswith(".pcap")
@@ -124,12 +123,12 @@ def test_generate(host: Host):
     assert "csv_basic_profile" in pcap and "csv_basic_profile" in csv
 
 
-def test_generate_cache(host: Host):
+def test_generate_cache(executor: Executor):
     """Test of generator cache. The same profile and the same config must result in the same PCAP.
     The same profile with modified configuration must result in different PCAP.
     """
 
-    generator = FtGenerator(host)
+    generator = FtGenerator(executor)
     pcap1, csv1 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
     pcap2, csv2 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
@@ -137,7 +136,7 @@ def test_generate_cache(host: Host):
 
     # force load from index pickle file
     del generator
-    generator = FtGenerator(host)
+    generator = FtGenerator(executor)
     pcap3, csv3 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
     assert pcap1 == pcap2 == pcap3 and csv1 == csv2 == csv3
@@ -172,17 +171,17 @@ def test_generate_cache(host: Host):
     assert pcap4 != pcap5 and csv4 != csv5
 
 
-def test_generate_cache_path(request: pytest.FixtureRequest, host: Host):
+def test_generate_cache_path(request: pytest.FixtureRequest, executor: Executor):
     """Test of generator cache when custom cache path is defined."""
 
     def finalizer():
-        host.run(f"rm -rf {temp_path}", path_replacement=False)
+        Tool(f"rm -rf {temp_path}", executor=executor).run()
 
     request.addfinalizer(finalizer)
 
-    temp_path = host.run("mktemp -d").stdout.strip()
+    temp_path = Tool("mktemp -d", executor=executor).run()[0].strip()
 
-    generator = FtGenerator(host, temp_path)
+    generator = FtGenerator(executor, temp_path)
     pcap1, csv1 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
     pcap2, csv2 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
@@ -192,14 +191,14 @@ def test_generate_cache_path(request: pytest.FixtureRequest, host: Host):
 
     # force new instance
     del generator
-    generator = FtGenerator(host, temp_path)
+    generator = FtGenerator(executor, temp_path)
     pcap3, csv3 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
     assert os.path.dirname(pcap3) == os.path.dirname(csv3) == temp_path
     assert pcap1 == pcap3 and csv1 == csv3
 
 
-def test_generate_cache_changed_profile(host: Host):
+def test_generate_cache_changed_profile(executor: Executor):
     """Test of generator cache. When content of profile file is modified, different PCAP must be returned."""
 
     with tempfile.TemporaryDirectory() as temp_path:
@@ -207,7 +206,7 @@ def test_generate_cache_changed_profile(host: Host):
 
         shutil.copy(os.path.join(INPUT_DIR, "csv_basic_profile.csv"), temp_profile)
 
-        generator = FtGenerator(host)
+        generator = FtGenerator(executor)
         pcap1, csv1 = generator.generate(temp_profile)
 
         # modify profile file, delete last row in temp_profile file
@@ -219,7 +218,7 @@ def test_generate_cache_changed_profile(host: Host):
         assert pcap1 != pcap2 and csv1 != csv2
 
 
-def test_generate_cache_changed_filename(host: Host):
+def test_generate_cache_changed_filename(executor: Executor):
     """Test of generator cache. Renamed profile file has the same hash but profile name is part of the key,
     so new files will be generated."""
 
@@ -227,7 +226,7 @@ def test_generate_cache_changed_filename(host: Host):
         temp_profile = os.path.join(temp_path, "renamed_profile.csv")
         shutil.copy(os.path.join(INPUT_DIR, "csv_basic_profile.csv"), temp_profile)
 
-        generator = FtGenerator(host)
+        generator = FtGenerator(executor)
         pcap1, csv1 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
         pcap2, csv2 = generator.generate(temp_profile)
 
@@ -236,85 +235,85 @@ def test_generate_cache_changed_filename(host: Host):
         assert "renamed_profile" in pcap2 and "renamed_profile" in csv2
 
 
-def test_generate_cache_consistency(host: Host):
+def test_generate_cache_consistency(executor: Executor):
     """Test of generator cache. When generated PCAP or CSV file changed or missing, files are regenerated."""
 
-    generator = FtGenerator(host)
+    generator = FtGenerator(executor)
     pcap1, csv1 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
     # modify result CSV file, remove last line
-    host.run(f"head -n -1 {csv1} > tmp.csv && mv tmp.csv {csv1}")
+    Tool(f"head -n -1 {csv1} > tmp.csv && mv tmp.csv {csv1}", executor=executor).run()
 
     # regenerate files because CSV hash does not match
     pcap2, csv2 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
     assert pcap1 != pcap2 and csv1 != csv2
-    assert host.run(f"test -f {pcap1}", check_rc=False).exited > 0
-    assert host.run(f"test -f {csv1}", check_rc=False).exited > 0
-    assert host.run(f"test -f {pcap2}", check_rc=False).exited == 0
-    assert host.run(f"test -f {csv2}", check_rc=False).exited == 0
+    assert not host_file_exist(executor, pcap1)
+    assert not host_file_exist(executor, csv1)
+    assert host_file_exist(executor, pcap2)
+    assert host_file_exist(executor, csv2)
 
     # modify result PCAP file, remove last line
-    host.run(f"head -n -1 {pcap2} > tmp.csv && mv tmp.csv {pcap2}")
+    Tool(f"head -n -1 {pcap2} > tmp.csv && mv tmp.csv {pcap2}", executor=executor).run()
 
     # regenerate files because PCAP hash does not match
     pcap3, csv3 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
     assert pcap2 != pcap3 and csv2 != csv3
-    assert host.run(f"test -f {pcap2}", check_rc=False).exited > 0
-    assert host.run(f"test -f {csv2}", check_rc=False).exited > 0
-    assert host.run(f"test -f {pcap3}", check_rc=False).exited == 0
-    assert host.run(f"test -f {csv3}", check_rc=False).exited == 0
+    assert not host_file_exist(executor, pcap2)
+    assert not host_file_exist(executor, csv2)
+    assert host_file_exist(executor, pcap3)
+    assert host_file_exist(executor, csv3)
 
     # remove CSV file
-    host.run(f"rm {csv3}")
+    Tool(f"rm {csv3}", executor=executor).run()
     # regenerate files because CSV missing
     pcap4, csv4 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
     assert pcap3 != pcap4 and csv3 != csv4
 
     # remove PCAP file
-    host.run(f"rm {pcap4}")
+    Tool(f"rm {pcap4}", executor=executor).run()
     # regenerate files because PCAP missing
     pcap5, csv5 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
     assert pcap4 != pcap5 and csv4 != csv5
 
 
-def test_generate_cache_move_dir(request: pytest.FixtureRequest, host: Host):
+def test_generate_cache_move_dir(request: pytest.FixtureRequest, executor: Executor):
     """Test of generator cache. Cache dir could be moved, path in index are relative."""
 
     def finalizer():
-        host.run(f"rm -rf {temp_path1}", path_replacement=False)
-        host.run(f"rm -rf {temp_path2}", path_replacement=False)
+        Tool(f"rm -rf {temp_path1}", executor=executor).run()
+        Tool(f"rm -rf {temp_path2}", executor=executor).run()
 
     request.addfinalizer(finalizer)
 
-    temp_path1 = host.run("mktemp -d").stdout.strip()
-    temp_path2 = host.run("mktemp -d").stdout.strip()
+    temp_path1 = Tool("mktemp -d", executor=executor).run()[0].strip()
+    temp_path2 = Tool("mktemp -d", executor=executor).run()[0].strip()
 
-    generator = FtGenerator(host, temp_path1)
+    generator = FtGenerator(executor, temp_path1)
     pcap1, csv1 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
     assert os.path.dirname(pcap1) == os.path.dirname(csv1) == temp_path1
 
     # move PCAP cache directory
-    host.run(f"cp {temp_path1}/* {temp_path2}", path_replacement=False)
-    host.run(f"rm -rf {temp_path1}", path_replacement=False)
+    Tool(f"cp {temp_path1}/* {temp_path2}", executor=executor).run()
+    Tool(f"rm -rf {temp_path1}", executor=executor).run()
 
-    generator = FtGenerator(host, temp_path2)
+    generator = FtGenerator(executor, temp_path2)
     pcap2, csv2 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
     assert os.path.dirname(pcap2) == os.path.dirname(csv2) == temp_path2
     assert os.path.basename(pcap1) == os.path.basename(pcap2) and os.path.basename(csv1) == os.path.basename(csv2)
-    assert host.run(f"test -f {pcap2}", check_rc=False).exited == 0
-    assert host.run(f"test -f {csv2}", check_rc=False).exited == 0
+    assert host_file_exist(executor, pcap2)
+    assert host_file_exist(executor, csv2)
 
 
 @pytest.mark.parametrize(
-    "host_stub",
+    "executor_stub",
     [(os.path.join(INPUT_DIR, "dummy.pcap"), os.path.join(INPUT_DIR, "csv_basic_report.csv"))],
     indirect=True,
 )
-def test_process_output(request: pytest.FixtureRequest, host_stub: HostStub):
+def test_process_output(request: pytest.FixtureRequest, executor_stub: ExecutorStub):
     """Test of CSV report processing. Biflows are splitted and column mapping for StatisticalModel is used."""
 
     def finalizer():
@@ -322,13 +321,10 @@ def test_process_output(request: pytest.FixtureRequest, host_stub: HostStub):
 
     request.addfinalizer(finalizer)
 
-    generator = FtGenerator(host_stub)
+    generator = FtGenerator(executor_stub)
     _, csv1 = generator.generate(os.path.join(INPUT_DIR, "csv_basic_profile.csv"))
 
-    if host_stub.is_local():
-        shutil.copy(csv1, FILE_DIR)
-    else:
-        host_stub.get_storage().pull(csv1, FILE_DIR)
+    Rsync(executor_stub).pull_path(csv1, FILE_DIR)
 
     assert filecmp.cmp(
         os.path.join(FILE_DIR, os.path.basename(csv1)), os.path.join(REF_DIR, "csv_basic_report_ref.csv")
