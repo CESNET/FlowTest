3b9839b5da965904ba4bb232846524fe007d99e3
Michal Sedlak <sedlakm@cesnet.cz>
2023-10-17 08:27:57 +0200

    ft-generator - fix Udp layer not reserving space for header

--- tools/ft-generator/src/layers/udp.cpp
+++ tools/ft-generator/src/layers/udp.cpp
@@ -11,6 +11,8 @@
 
 #include <pcapplusplus/UdpLayer.h>
 
+static constexpr int UDP_HDR_LEN = sizeof(pcpp::udphdr);
+
 namespace generator {
 
 Udp::Udp(uint16_t portSrc, uint16_t portDst)
@@ -25,6 +27,7 @@ void Udp::PlanFlow(Flow& flow)
 
 	for (auto& packet : packetsSpan) {
 		Packet::layerParams params;
+		packet._size += UDP_HDR_LEN;
 		packet._layers.emplace_back(std::make_pair(this, params));
 	}
 }
@@ -40,6 +43,9 @@ void Udp::Build(PcppPacket& packet, Packet::layerParams& params, Packet& plan)
 	} else {
 		udpLayer = new pcpp::UdpLayer(_portDst, _portSrc);
 	}
+
+	assert(plan._size >= UDP_HDR_LEN);
+	plan._size -= UDP_HDR_LEN;
 
 	packet.addLayer(udpLayer, true);
 }
