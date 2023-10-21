476d39b792437aa04107bd4716c302554d96494a
Michal Sedlak <sedlakm@cesnet.cz>
2023-10-21 09:11:13 +0200

    ft-generator - introduce Payload configuration section class

---
+++ tools/ft-generator/src/config/payload.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Payload config section
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "payload.h"
+#include "common.h"
+
+#include <map>
+
+namespace generator {
+namespace config {
+
+static PayloadProtocol ParsePayloadProtocol(const YAML::Node& node)
+{
+	static std::map<std::string, PayloadProtocol> protocolNameToProtocol {
+		{"http", PayloadProtocol::Http},
+		{"dns", PayloadProtocol::Dns}};
+	static std::string protocolNames = StringJoin(KeysOfMap(protocolNameToProtocol), ", ");
+
+	std::string value = AsScalar(node);
+	auto it = protocolNameToProtocol.find(value);
+	if (it == protocolNameToProtocol.end()) {
+		throw ConfigError(
+			node,
+			"invalid protocol name " + value + ", expected one of: " + protocolNames);
+	}
+
+	return it->second;
+}
+
+PayloadProtocolList::PayloadProtocolList(std::initializer_list<PayloadProtocol> values)
+	: _values(values)
+{
+}
+
+PayloadProtocolList::PayloadProtocolList(const YAML::Node& node)
+{
+	ExpectSequence(node);
+	for (const auto& subnode : node) {
+		ExpectScalar(subnode);
+		auto value = ParsePayloadProtocol(subnode);
+		if (Includes(value)) {
+			throw ConfigError(node, "value " + AsScalar(subnode) + " specified multiple times");
+		}
+		_values.emplace(value);
+	}
+}
+
+bool PayloadProtocolList::Includes(PayloadProtocol value) const
+{
+	return _values.find(value) != _values.end();
+}
+
+Payload::Payload(const YAML::Node& node)
+{
+	CheckAllowedKeys(node, {"enabled_protocols"});
+
+	if (node["enabled_protocols"].IsDefined()) {
+		_enabledProtocols = PayloadProtocolList(node["enabled_protocols"]);
+	}
+}
+
+} // namespace config
+} // namespace generator
---
+++ tools/ft-generator/src/config/payload.h
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * @author Michal Sedlak <sedlakm@cesnet.cz>
+ * @brief Payload config section
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <yaml-cpp/yaml.h>
+
+#include <initializer_list>
+#include <set>
+
+namespace generator {
+namespace config {
+
+/**
+ * @brief The possible payload protocols
+ */
+enum class PayloadProtocol { Http, Dns };
+
+/**
+ * @brief A list of payload protocols
+ */
+class PayloadProtocolList {
+public:
+	/**
+	 * @brief Construct the list from specified values
+	 *
+	 * @param values The values
+	 */
+	PayloadProtocolList(std::initializer_list<PayloadProtocol> values);
+
+	/**
+	 * @brief Parse the protocol list from a yaml
+	 *
+	 * @param node The yaml node to parse
+	 */
+	PayloadProtocolList(const YAML::Node& node);
+
+	/**
+	 * @brief Check if the list includes a specific value
+	 */
+	bool Includes(PayloadProtocol value) const;
+
+private:
+	std::set<PayloadProtocol> _values;
+};
+
+/**
+ * @brief Payload configuration section
+ */
+class Payload {
+public:
+	/**
+	 * @brief Payload configuration section with default values
+	 */
+	Payload() = default;
+
+	/**
+	 * @brief Parse the payload configuration section node from a yaml
+	 *
+	 * @param node The yaml node to parse
+	 */
+	Payload(const YAML::Node& node);
+
+	/**
+	 * @brief Get the set of enabled protocols
+	 */
+	const PayloadProtocolList& GetEnabledProtocols() const { return _enabledProtocols; }
+
+private:
+	PayloadProtocolList _enabledProtocols = {PayloadProtocol::Http, PayloadProtocol::Dns};
+};
+
+} // namespace config
+} // namespace generator
