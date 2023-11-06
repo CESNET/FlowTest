CESNET/FlowTest

commit d4d9eea5c7eb47c0741a3c073a87f277ad72dec4

ft-orchestration - ft_replay - introduce mtu parameter In case of usage raw or xdp output plugin is mtu set by ip link.

tools/ft-orchestration/src/generator/ft_replay.py
@@ -48,34 +48,25 @@ class FtReplayOutputPluginSettings:
         Number of output queues. Not relevant with raw plugin.
     burst_size: int, optional
         Size of burst of outgoing packets.
-    packet_size: int, optional
-        Maximal size (Bytes) of single packet. Not usable with nfb plugin.
     super_packet: str, optional
         Enable Super Packet feature (merging of small packets into larger ones).
         Value: no/yes/auto. Only for nfb plugin.
-    super_packet_size: int, optional
-        Maximal size (Bytes) of single Super Packet.
     """
 
     output_plugin: str = "raw"
     queue_count: int = 1
     burst_size: Optional[int] = None
-    packet_size: Optional[int] = None
     super_packet: Optional[str] = None
-    super_packet_size: Optional[int] = None
 
     def __post_init__(self) -> None:
         """Check combination of input plugin and parameters."""
 
         err = False
         if self.output_plugin == "raw":
-            if self.queue_count > 1 or self.super_packet or self.super_packet_size:
-                err = True
-        elif self.output_plugin == "nfb":
-            if self.packet_size:
+            if self.queue_count > 1 or self.super_packet:
                 err = True
         elif self.output_plugin in ["xdp", "pcapFile"]:
-            if self.super_packet or self.super_packet_size:
+            if self.super_packet:
                 err = True
         else:
             logging.getLogger().error("FtReplay: Used unknown output plugin '%s'", self.output_plugin)
@@ -85,7 +76,7 @@ class FtReplayOutputPluginSettings:
             logging.getLogger().error("FtReplay: Used unsupported %s output plugin parameters.", self.output_plugin)
             raise RuntimeError(f"FtReplay: Used unsupported {self.output_plugin} output plugin parameters.")
 
-    def get_cmd_args(self, interface: str) -> str:
+    def get_cmd_args(self, interface: str, mtu: int) -> str:
         """Get output plugin argument for ft-replay.
 
         Parameters
@@ -104,12 +95,13 @@ class FtReplayOutputPluginSettings:
             args.append(f"queueCount={self.queue_count}")
         if self.burst_size:
             args.append(f"burstSize={self.burst_size}")
-        if self.packet_size:
-            args.append(f"packetSize={self.packet_size}")
         if self.super_packet:
             args.append(f"superPacket={self.super_packet}")
-        if self.super_packet_size:
-            args.append(f"superPacketSize={self.super_packet_size}")
+
+        if self.output_plugin in ["raw", "xdp", "pcapFile"]:
+            args.append(f"packetSize={mtu}")
+        if self.output_plugin == "nfb":
+            args.append(f"superPacketSize={mtu}")
 
         if self.output_plugin in ["raw", "xdp"]:
             args.append(f"ifc={interface}")
@@ -182,6 +174,7 @@ class FtReplay(Replicator):
         verbose: bool = False,
         biflow_export: bool = False,
         cache_path: str = None,
+        mtu: int = 1522,
         **kwargs: dict,
     ) -> None:
         """FtReplay initializer.
@@ -201,6 +194,9 @@ class FtReplay(Replicator):
             report contains min(START_TIME,START_TIME_REV) resp. max(END_TIME,END_TIME_REV).
         cache_path : str, optional
             Custom cache path for PCAPs generated by ft-generator on (remote) host.
+        mtu: int, optional
+            Mtu size of interface on which traffic will be replayed.
+            Default size is defined by standard MTU with ethernet and VLAN header.
         kwargs : dict
             Additional arguments used to set up ft-replay output plugin.
 
@@ -217,6 +213,7 @@ class FtReplay(Replicator):
         self._interface = None
         self._dst_mac = None
         self._process = None
+        self._mtu = mtu
 
         self._replication_units = []
         self._srcip_offset = None
@@ -409,11 +406,12 @@ class FtReplay(Replicator):
         cmd_args += [self._get_speed_arg(speed)]
         if self._vlan:
             cmd_args += ["-v", str(self._vlan)]
-        cmd_args += ["-o", self._output_plugin.get_cmd_args(self._interface)]
+        cmd_args += ["-o", self._output_plugin.get_cmd_args(self._interface, self._mtu)]
         cmd_args += ["-i", pcap_path]
 
         if self._output_plugin.output_plugin in ["raw", "xdp"]:
             self._host.run(f"sudo ip link set dev {self._interface} up")
+            self._host.run(f"sudo ip link set dev {self._interface} mtu {self._mtu}")
 
         logging.getLogger().info("Traffic transmission started.")
         start = time.time()

commit 1a61a1c686bb570e1f10566b7b82e836343782f7

ft-orchestration - ft_replay - fix nfb output plugin handling Bug has been introduced in commit d4d9eea5c7eb47c0741a3c073a87f277ad72dec4

tools/ft-orchestration/src/generator/ft_replay.py
@@ -68,6 +68,8 @@ class FtReplayOutputPluginSettings:
         elif self.output_plugin in ["xdp", "pcapFile"]:
             if self.super_packet:
                 err = True
+        elif self.output_plugin == "nfb":
+            pass
         else:
             logging.getLogger().error("FtReplay: Used unknown output plugin '%s'", self.output_plugin)
             raise RuntimeError(f"FtReplay: Used unknown output plugin '{self.output_plugin}'")