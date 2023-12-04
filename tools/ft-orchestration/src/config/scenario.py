9060a1101c0e1f8307131d71be4f66477710a3dc
Tomas Jansky <Tomas.Jansky@progress.com>
2023-12-04 10:19:46 +0100

    ft-orchestration - add support to define multiple tests in a single scenario configuration

--- tools/ft-orchestration/src/config/scenario.py
+++ tools/ft-orchestration/src/config/scenario.py
@@ -7,18 +7,20 @@ SPDX-License-Identifier: BSD-3-Clause
 Configuration of individual test scenarios.
 """
 
+import copy
 import hashlib
 import logging
 import os
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, Union
 from urllib.parse import urlparse
 from urllib.request import HTTPError, urlretrieve
 
 from dataclass_wizard import YAMLWizard
-from ftanalyzer.models.sm_data_types import SMMetric
+from ftanalyzer.models.sm_data_types import SMMetric, SMMetricType
 from src.common.required_field import required_field
 from src.generator.ft_generator import FtGeneratorConfig
+from src.generator.interface import MbpsSpeed, MultiplierSpeed, PpsSpeed
 
 
 class ScenarioCfgException(Exception):
@@ -75,13 +77,147 @@ class ProbeCfg(YAMLWizard):
 
 
 @dataclass
+class AnalysisCfg(YAMLWizard):
+    """Configuration of a test scenario evaluation."""
+
+    model: str = "statistical"
+    metrics: list[SMMetric] = field(
+        default_factory=lambda: [SMMetric(SMMetricType.PACKETS, 0.0001), SMMetric(SMMetricType.BYTES, 0.0001)]
+    )
+
+
+@dataclass
+class SimTest(YAMLWizard):
+    """Mandatory configuration for all individual tests in simulation test scenarios."""
+
+    id: str = required_field()
+    enabled: bool = True
+    marks: list[str] = field(default_factory=list)
+
+
+@dataclass
+class SimConfig(YAMLWizard):
+    """Basic configurable options for simulation tests."""
+
+    pps: Optional[int] = None
+    mbps: Optional[int] = None
+    speed_multiplier: Optional[float] = None
+    probe: Optional[ProbeCfg] = None
+    generator: Optional[FtGeneratorConfig] = None
+
+    def get_probe_conf(self, probe_type: str, default: ProbeCfg) -> dict:
+        """Get probe configuration for a specific probe type (merged with the default configuration).
+
+        Parameters
+        ----------
+        probe_type: str
+            Probe type.
+        default: ProbeCfg
+            Default probe configuration.
+
+        Returns
+        -------
+        dict
+            ProbeCfg configuration for the specified probe type.
+        """
+
+        probe_conf = default.get_args(probe_type)
+        if self.probe is None:
+            return probe_conf
+
+        return {**probe_conf, **self.probe.get_args(probe_type)}
+
+    def get_generator_conf(self, default: FtGeneratorConfig) -> FtGeneratorConfig:
+        """Get generator configuration (merged with the default configuration).
+
+        Parameters
+        ----------
+        default: FtGeneratorConfig
+            Default generator configuration.
+
+        Returns
+        -------
+        FtGeneratorConfig
+            Generator configuration.
+        """
+
+        if self.generator is not None:
+            default.update(self.generator)
+
+        return default
+
+    def get_replay_speed(self, default: "SimConfig") -> Union[MbpsSpeed, MultiplierSpeed, PpsSpeed]:
+        """
+        Determine replay speed from the provided configuration.
+        Priorities:
+            test mbps -> test pps -> test multiplier ->
+            -> default mbps -> default pps -> default multiplier -> multiplier 1.0
+
+        Parameters
+        ----------
+        default: SimConfig
+            Default configuration.
+
+        Returns
+        -------
+        MbpsSpeed, MultiplierSpeed, PpsSpeed
+            Replay speed.
+        """
+
+        # pylint: disable=too-many-return-statements
+        if self.mbps is not None:
+            return MbpsSpeed(self.mbps)
+
+        if self.pps is not None:
+            return PpsSpeed(self.pps)
+
+        if self.speed_multiplier is not None:
+            return MultiplierSpeed(self.speed_multiplier)
+
+        if default.mbps is not None:
+            return MbpsSpeed(default.mbps)
+
+        if default.pps is not None:
+            return PpsSpeed(default.pps)
+
+        if default.speed_multiplier is not None:
+            return MultiplierSpeed(default.speed_multiplier)
+
+        return MultiplierSpeed(1.0)
+
+
+@dataclass
+class SimGeneral(SimTest, SimConfig):
+    """Configuration of an individual test in general simulation test scenario."""
+
+    loops: int = 1
+    replicator: Optional[int] = None
+    analysis: AnalysisCfg = AnalysisCfg()
+
+    def get_replicator_units(self, sampling: float) -> int:
+        """Get number of replicator units (either directly configured or based on sampling value).
+
+        Parameters
+        ----------
+        sampling: float
+            Sampling value used to create the profile.
+
+        Returns
+        -------
+        int
+            Number of replicator units.
+        """
+
+        return self.replicator if self.replicator is not None else int(1 / sampling)
+
+
+@dataclass
 class ScenarioCfg(YAMLWizard):
     """Test scenario configuration."""
 
     name: str = required_field()
     description: str = required_field()
     marks: list[str] = required_field()
-    exclude: list[str] = field(default_factory=list)
     requirements: Requirements = Requirements()
 
     def check(self) -> None:
@@ -96,13 +232,71 @@ class SimulationScenario(ScenarioCfg):
     """Simulation testing scenario configuration."""
 
     profile: str = required_field()
-    mtu: int = required_field()
-    pps: int = required_field()
     sampling: float = required_field()
-    analysis: list[SMMetric] = required_field()
+    mtu: int = required_field()
+    default: SimConfig = required_field()
 
-    probe: Optional[ProbeCfg] = ProbeCfg()
-    generator: Optional[FtGeneratorConfig] = FtGeneratorConfig()
+    # attributes which are not read from configuration but runtime
+    filename: Optional[str] = None
+    test: Optional[SimTest] = None
+
+    # configuration of individual tests for different simulation scenarios
+    sim_general: Optional[list[SimGeneral]] = None
+
+    def check(self) -> None:
+        """Check validity of the configuration."""
+
+        super().check()
+
+        if self.default.probe is None:
+            raise ScenarioCfgException("Missing default probe configuration.")
+
+        if self.default.generator is None:
+            raise ScenarioCfgException("Missing default generator configuration.")
+
+    def get_tests(self, filename: str, name: Optional[str]) -> list[tuple]:
+        """
+        Get all tests configuration for a specific simulation test scenario.
+        If the simulation scenario supports configuration of specific tests, the test configuration is
+        added into the main configuration under attribute "test".
+
+        Parameters
+        ----------
+        filename: str
+            Path to the scenario configuration filename. Added to the configuration as "filename" attribute.
+            Can be used as test id in case the particular simulation scenario
+            doesn't support individual tests configuration.
+        name: str, None
+            Name of the simulation test scenario.
+            If the name is not provided, the "test" attribute of the scenario configuration is set to None
+            and filename is used in place of the test id.
+
+        Returns
+        -------
+        list
+            Individual tests for the specified test scenario.
+            Tests consists of a tuple (configuration, marks, id).
+        """
+
+        if name is None:
+            cfg = copy.deepcopy(self)
+            cfg.filename = filename
+            return [(cfg, cfg.marks, filename.removesuffix(".yml"))]
+
+        if getattr(self, name, None) is None:
+            return []
+
+        tests = []
+        for test in getattr(self, name):
+            if not test.enabled:
+                continue
+
+            cfg = copy.deepcopy(self)
+            cfg.test = test
+            cfg.filename = filename
+            tests.append((cfg, cfg.marks + test.marks + [name], test.id))
+
+        return tests
 
     def get_profile(self, scenario_filename: str, simulation_tests_dir: str) -> str:
         """
@@ -159,3 +353,20 @@ class ValidationScenario(ScenarioCfg):
     key: Optional[list[str]] = None
     at_least_one: Optional[list[str]] = None
     probe: Optional[ProbeCfg] = ProbeCfg()
+
+    def get_tests(self, filename: str, *_) -> list[tuple]:
+        """
+        Get validation test scenario.
+
+        Parameters
+        ----------
+        filename: str
+            Used as test id.
+
+        Returns
+        -------
+        list
+            Validation test scenario configuration, marks, id.
+        """
+
+        return [(self, self.marks, filename.removesuffix(".yml"))]
