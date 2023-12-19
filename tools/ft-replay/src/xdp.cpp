CESNET/FlowTest

commit 9e1ca6f8cd00a07a90c6dd358219de786b0f4fcc

ft-replay - introduce termination timeout of XDP queues
Sometimes, network drivers do not return all transmitted
packets before the queue is closed. This commit implements
timeout to avoid endless waiting.

tools/ft-replay/src/xdp.cpp
@@ -11,6 +11,7 @@
 #include "ethTool.hpp"
 
 #include <arpa/inet.h>
+#include <chrono>
 #include <linux/if_link.h>
 #include <linux/if_packet.h>
 #include <linux/sockios.h>
@@ -20,6 +21,8 @@
 #include <unistd.h>
 
 namespace replay {
+
+constexpr auto QUEUE_FLUSH_TIMEOUT = 3s;
 
 Umem::Umem(const QueueConfig& cfg)
 	: _bufferSize(cfg._packetSize * cfg._umemSize)
@@ -126,7 +129,8 @@ int XdpSocket::GetDescriptor()
 }
 
 XdpQueue::XdpQueue(const QueueConfig& cfg, size_t id)
-	: _pktSize(cfg._packetSize)
+	: _id(id)
+	, _pktSize(cfg._packetSize)
 	, _bufferSize(cfg._umemSize * cfg._packetSize)
 	, _burstSize(cfg._burstSize)
 	, _lastBurstTotalPacketLen(0)
@@ -144,8 +148,16 @@ XdpQueue::XdpQueue(const QueueConfig& cfg, size_t id)
 
 XdpQueue::~XdpQueue()
 {
-	while (_outstandingTx != 0) {
+	auto start {std::chrono::steady_clock::now()};
+	auto end = start;
+
+	while (_outstandingTx != 0 && end - start > QUEUE_FLUSH_TIMEOUT) {
 		CollectSlots();
+		end = std::chrono::steady_clock::now();
+	}
+
+	if (_outstandingTx > 0) {
+		_logger->warn("Queue (ID {}) did not return all remaining packet buffers.", _id);
 	}
 }
 

tools/ft-replay/src/xdp.hpp
@@ -189,6 +189,7 @@ public:
 private:
 	void CollectSlots();
 
+	size_t _id;
 	size_t _pktSize;
 	size_t _bufferSize;
 	size_t _burstSize;

commit 501fec6946bb8675093ec0560b52cd9b311268ca

ft-replay - fix invalid time comparison in XDP output
Fixes: 9e1ca6f8cd00

tools/ft-replay/src/xdp.cpp
@@ -151,7 +151,7 @@ XdpQueue::~XdpQueue()
 	auto start {std::chrono::steady_clock::now()};
 	auto end = start;
 
-	while (_outstandingTx != 0 && end - start > QUEUE_FLUSH_TIMEOUT) {
+	while (_outstandingTx != 0 && end - start < QUEUE_FLUSH_TIMEOUT) {
 		CollectSlots();
 		end = std::chrono::steady_clock::now();
 	}